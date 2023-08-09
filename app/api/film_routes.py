433868f0220e973508ad1e23ab1febe077eae98b
Alex <alex.j.basso@gmail.com>
2023-08-08 19:02:20 -0500

    get film by id route

--- app/api/film_routes.py
+++ app/api/film_routes.py
@@ -10,3 +10,11 @@ def get_all_films():
     Query for all films and returns them in a list of film dictionaries
     """
     return jsonify([film.to_dict() for film in Film.query.all()])
+
+@film_routes.route("/<int:id>")
+def get_film_by_id(id):
+    """
+    Query for a film by id and returns that film in a dictionary
+    """
+    film = Film.query.get(id)
+    return jsonify(film.to_dict())

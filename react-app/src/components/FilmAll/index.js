1eaf5cc9dc86f334b390c5a32fb1a4720ae7eac0
Alex <alex.j.basso@gmail.com>
2023-08-11 17:04:41 -0500

    all films loaded

--- react-app/src/components/FilmAll/index.js
+++ react-app/src/components/FilmAll/index.js
@@ -1,20 +1,28 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { getAllFilmsThunk } from '../../store/films';
+import "./FilmAll.css"
 
 export default function FilmAll() {
   const dispatch = useDispatch();
-  const allFilms = useSelector(state => state.films.allFilms)
+  const allFilms = Object.values(useSelector(state => state.films.allFilms))
 
   useEffect(() => {
     dispatch(getAllFilmsThunk());
   }, [dispatch]);
 
-  console.log("allFilms:", Object.values(allFilms))
+  console.log("allFilms:", (allFilms))
 
   return (
     <div id="film-all-container">
-      <h1>All Films</h1>
+      <span>FILMS</span>
+      <span>There are {allFilms.length} films.</span>
+      <div id="all-films-grid">
+        {allFilms.map(film =>
+          <a key={film.id} href={`film/${film.id}`}>
+            <img src={film.key_art} />
+          </a>)}
+      </div>
     </div>
   )
 }

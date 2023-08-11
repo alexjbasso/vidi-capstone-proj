731446298e632b71beeb66f7357c574a5e1e30c4
Alex <alex.j.basso@gmail.com>
2023-08-11 13:15:22 -0500

    cast/crew toggle

--- react-app/src/components/FilmDetails/index.js
+++ react-app/src/components/FilmDetails/index.js
@@ -8,26 +8,22 @@ export default function FilmDetails() {
   const dispatch = useDispatch();
   const { id } = useParams();
   const film = useSelector(state => state.films.singleFilm[id])
-  const [toggledRole, setToggledRole] = useState("Actor")
-  const [filteredPeople, setFilteredPeople] = useState([])
+  const [toggledRole, setToggledRole] = useState("CAST")
 
   useEffect(() => {
     dispatch(getFilmByIdThunk(id));
   }, [dispatch, id]);
 
-// if toggled role == Actor or Actress then set to cast, else set to crew
-
-  useEffect(() => {
-    if (film) {
-      setFilteredPeople(film?.roles.filter(role => role.role === toggledRole))
-    }
-  }, [dispatch, film, toggledRole])
-
 
   if (!film) return <h1>Film not found.</h1>
 
-  // console.log(film)
-  // console.log(filteredPeople[0])
+  const cast = film.roles.filter(person => person.role === 'Actress' || person.role === 'Actor')
+  const directors = film.roles.filter(person => person.role === 'Director')
+  const writers = film.roles.filter(person => person.role === 'Writer')
+  const cines = film.roles.filter(person => person.role === 'Cinematographer')
+  const editors = film.roles.filter(person => person.role === 'Editor')
+  const composers = film.roles.filter(person => person.role === 'Composer')
+
   return (
     <div id="film-details-page-container">
 
@@ -38,19 +34,71 @@ export default function FilmDetails() {
       <div id="details-right-cont">
         <div id="film-details-top">
           <div id="film-details-cont">
-            <h3>{film.title}</h3>
+            <h2 id="title-heading">{film.title}</h2>
             <h4>{film.year} Directed by {film.roles.find(role => role.role === 'Director')?.name}</h4>
             <p>{film.synopsis}</p>
             <div id="credits-cont">
-              <span>
+              <span className="credit-toggle"
+              onClick={() => setToggledRole("CAST")}
+              style={{ color: toggledRole === "CAST" ? "rgb(0, 224, 84)" : "white" }}>
                 CAST
               </span>
-              <span>
+              <span className="credit-toggle"
+              onClick={() => setToggledRole("CREW")}
+              style={{ color: toggledRole === "CREW" ? "rgb(0, 224, 84)" : "white" }}>
                 CREW
               </span>
             </div>
             <div id="credits-block">
-              {filteredPeople.map(person => <a>{person.name}</a>)}
+
+              {toggledRole === "CAST" ? <div id="cast-block">
+                {cast.map(person => <a key={person.name} href={`/person/${person.person_id}`}>{person.name}</a>)}
+              </div> : null}
+
+              {toggledRole === "CREW" ? <div id="crew-block">
+
+                {directors.length ?
+                  <div className="crew-line">
+                    <span className="role-text">DIRECTOR</span>
+                    <div>
+                      {directors.map(person => <a key={person.name} href={`/person/${person.person_id}`}>{person.name}</a>)}
+                    </div>
+                  </div> : null}
+
+                {writers.length ?
+                  <div className="crew-line">
+                    <span className="role-text">{writers.length > 1 ? "WRITERS" : "WRITER"}</span>
+                    <div>
+                      {writers.map(person => <a key={person.name} href={`/person/${person.person_id}`}>{person.name}</a>)}
+                    </div>
+                  </div> : null}
+
+                {editors.length ?
+                  <div className="crew-line">
+                    <span className="role-text">{editors.length > 1 ? "EDITORS" : "EDITOR"}</span>
+                    <div>
+                      {editors.map(person => <a key={person.name} href={`/person/${person.person_id}`}>{person.name}</a>)}
+                    </div>
+                  </div> : null}
+
+                {cines.length ?
+                  <div className="crew-line">
+                    <span className="role-text">CINEMATOGRAPHY</span>
+                    <div>
+                      {cines.map(person => <a key={person.name} href={`/person/${person.person_id}`}>{person.name}</a>)}
+                    </div>
+                  </div> : null}
+
+                {composers.length ?
+                  <div className="crew-line">
+                    <span className="role-text">{composers.length > 1 ? "COMPOSERS" : "COMPOSER"}</span>
+                    <div>
+                      {composers.map(person => <a key={person.name} href={`/person/${person.person_id}`}>{person.name}</a>)}
+                    </div>
+                  </div> : null}
+
+
+              </div> : null}
             </div>
             <p>{film.duration} mins</p>
           </div>

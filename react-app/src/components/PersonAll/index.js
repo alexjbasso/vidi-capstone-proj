8d280d03bd7de471f8b3d7ee51e3ac302a702556
Alex <alex.j.basso@gmail.com>
2023-08-14 22:56:39 -0500

    style profile button

--- react-app/src/components/PersonAll/index.js
+++ react-app/src/components/PersonAll/index.js
@@ -1,20 +1,36 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPeopleThunk } from '../../store/people';
+import "./PersonAll.css"
 
 export default function PersonAll() {
   const dispatch = useDispatch();
-  const allPeople = useSelector(state => state.people.allPeople)
+  const people = Object.values(useSelector(state => state.people?.allPeople))
+
+  people.sort((a, b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+  });
 
   useEffect(() => {
     dispatch(getAllPeopleThunk());
   }, [dispatch]);
 
-  console.log("allPeople:", Object.values(allPeople))
+  console.log(people)
 
   return (
     <div id="person-all-container">
-      <h1>All People</h1>
+      <span id="all-people-header">PEOPLE<a href="person/new"><i className="fa fa-plus add-person"></i></a> </span>
+      <span id="person-count">There are {people.length} people.</span>
+      <div id="all-people-grid">
+        {people.map(person =>
+          <a key={person.id} href={`person/${person.id}`}>
+            <img src={person.featured_photo} />
+          </a>)}
+      </div>
     </div>
   )
 }

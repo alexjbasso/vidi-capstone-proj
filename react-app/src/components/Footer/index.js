fb5e502bbbbe33b13283d308d547d817a06b3ecf
Alex <alex.j.basso@gmail.com>
2023-08-15 19:45:57 -0500

    footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,7 +1,37 @@
+import "./Footer.css";
+
 export default function Footer() {
 
   return (
-    <h1>Footer goe heres</h1>
+    <div id="footer-container">
+      <div id="footer-content">
+
+        <div className="footer-col" id="creator">
+          <i className="fa fa-user footer-header"></i>
+          <span>Alex Basso</span>
+          <a href="https://www.linkedin.com/in/alexjbasso/">–LinkedIn</a>
+          <a href="https://github.com/alexjbasso">–Github</a>
+        </div>
+
+        <div className="footer-col" id="languages">
+          <i className="fa fa-code footer-header"></i>
+          <span>JavaScript</span>
+          <span>React</span>
+          <span>Redux</span>
+          <span>Python</span>
+          <span>Flask</span>
+          <span>AWS</span>
+        </div>
+
+        <div className="footer-col" id="for">
+          <i className="fa fa-school footer-header"></i>
+          <span>App Academy</span>
+          <span>August 2023</span>
+          <a href="https://github.com/alexjbasso/vidi-capstone-proj">v1.0</a>
+        </div>
+
+      </div>
+    </div>
   )
 
 }

23d8ed6651da2286c85f04137c75371398a3163c
Alex <alex.j.basso@gmail.com>
2023-08-17 18:08:01 -0500

    reviews store

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -3,11 +3,13 @@ import thunk from 'redux-thunk';
 import session from './session'
 import filmsReducer from './films';
 import peopleReducer from './people';
+import reviewsReducer from './reviews';
 
 const rootReducer = combineReducers({
   session,
   films: filmsReducer,
-  people: peopleReducer
+  people: peopleReducer,
+  reviews: reviewsReducer
 });
 
 
---
+++ react-app/src/store/reviews.js
@@ -0,0 +1,176 @@
+// ===Action Types===
+const LOAD_REVIEWS = 'reviews/LOAD_REVIEWS';
+const LOAD_REVIEW = 'reviews/LOAD_REVIEW';
+const LOAD_USER_REVIEWS = 'reviews/LOAD_USER_REVIEWS';
+const ADD_REVIEW = 'reviews/ADD_REVIEW';
+const EDIT_REVIEW = 'reviews/EDIT_REVIEW';
+const DELETE_REVIEW = 'reviews/DELETE_REVIEW';
+const CLEAR_REVIEWS = 'reviews/CLEAR_REVIEWS';
+
+// ===Action Creators===
+// Get All Reviews Action
+export const getAllReviewsAction = (reviews) => {
+  return {
+    type: LOAD_REVIEWS,
+    payload: reviews,
+  };
+};
+
+// Get All User's Reviews Action
+export const getAllUserReviewsAction = (reviews) => {
+  return {
+    type: LOAD_USER_REVIEWS,
+    payload: reviews,
+  };
+};
+
+// Get Review by ID Action
+export const getReviewByIdAction = (review) => {
+  return {
+    type: LOAD_REVIEW,
+    payload: review,
+  };
+};
+
+// Create Review Action
+export const addReviewAction = (review) => {
+  return {
+    type: ADD_REVIEW,
+    payload: review,
+  };
+};
+
+
+// Edit a Review Action
+export const editReviewAction = (review) => {
+  return {
+    type: EDIT_REVIEW,
+    payload: review,
+  };
+};
+
+// Delete a Review Action
+export const deleteReviewAction = (reviewId) => {
+  return {
+    type: DELETE_REVIEW,
+    payload: reviewId,
+  };
+};
+
+// Clear Reviews State Action
+export const clearReviewsAction = () => {
+  return { type: CLEAR_REVIEWS }
+};
+
+
+// ===Thunks===
+// Get All Reviews Thunk
+export const getAllReviewsThunk = () => async (dispatch) => {
+  const response = await fetch('/api/reviews');
+  const reviews = await response.json();
+  dispatch(getAllReviewsAction(reviews));
+  return response;
+};
+
+// Get All Reviews of Current User Thunk
+export const getAllReviewsOfUserThunk = () => async (dispatch) => {
+  const response = await fetch('/api/reviews/current');
+  const reviews = await response.json();
+  dispatch(getAllUserReviewsAction(reviews));
+  return response;
+};
+
+// Get Review by ID Thunk
+export const getReviewByIdThunk = (reviewId) => async (dispatch) => {
+  const response = await fetch(`/api/reviews/${reviewId}`);
+  const review = await response.json();
+  if (response.ok) {
+    dispatch(getReviewByIdAction(review));
+  }
+  return review;
+};
+
+// Add a Review Thunk
+export const addReviewThunk = (formData) => async (dispatch) => {
+  try {
+    const response = await fetch('/api/reviews/new', {
+      method: 'POST',
+      body: formData,
+    });
+    const newReview = await response.json();
+    if (!response.ok) {
+      throw new Error(newReview)
+    }
+    return dispatch(addReviewAction(newReview))
+  } catch (err) {
+    return err
+  }
+};
+
+//Edit a Review Thunk
+export const editReviewThunk = (review, formData) => async (dispatch) => {
+  try {
+    const response = await fetch(`/api/reviews/${review.id}/edit`, {
+      method: 'PUT',
+      body: formData,
+    });
+    const editedReview = await response.json();
+    if (!response.ok) {
+      throw new Error(editedReview)
+    }
+    return dispatch(editReviewAction(editedReview))
+  } catch (err) {
+    return err
+  }
+}
+
+//Delete a Review Thunk
+export const deleteReviewThunk = (reviewId) => async (dispatch) => {
+  const response = await fetch(`/api/reviews/${reviewId}/delete`, {
+    method: 'DELETE',
+  });
+
+  if (response.ok) {
+    dispatch(deleteReviewAction(reviewId));
+    return response;
+  }
+};
+
+
+// ===Reducer===
+const initialState = {
+  allReviews: {},
+  allUserReviews: {},
+  singleReview: {}
+}
+
+export default function reviewsReducer(state = initialState, action) {
+  switch (action.type) {
+    case LOAD_REVIEWS:
+      const allReviewsObject = {};
+      action.payload.forEach((review) => {
+        allReviewsObject[review.id] = review;
+      });
+      return { ...state, allReviews: allReviewsObject };
+    case LOAD_USER_REVIEWS:
+      const allUserReviewsObject = {};
+      action.payload.forEach((review) => {
+        allUserReviewsObject[review.id] = review;
+      });
+      return { ...state, allUserReviews: allUserReviewsObject };
+    case LOAD_REVIEW:
+      return { ...state, singleReview: { [action.payload.id]: action.payload } };
+    case ADD_REVIEW:
+      return { ...state, allReviews: { ...state.allReviews, [action.payload.id]: action.payload } };
+    case EDIT_REVIEW:
+      return { ...state, singleReview: { [action.payload.id]: action.payload } };
+    case DELETE_REVIEW:
+      const allReviewsObj = { ...state.allReviews };
+      delete allReviewsObj[action.payload];
+      return { ...state, allReviews: allReviewsObj };
+    case CLEAR_REVIEWS:
+      return {};
+    default:
+      return state;
+  }
+}

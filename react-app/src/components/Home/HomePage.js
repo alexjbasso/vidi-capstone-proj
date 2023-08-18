9962093ec159cbfdc90a5f8947d028e925d28d22
Alex <alex.j.basso@gmail.com>
2023-08-18 16:16:02 -0500

    ad style

--- react-app/src/components/Home/HomePage.js
+++ react-app/src/components/Home/HomePage.js
@@ -27,7 +27,7 @@ export default function HomePage({ user, films, people }) {
           </div>
         </div>
 
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="https://a.ltrbxd.com/resized/sm/upload/j8/gt/r8/ss/pro-950-0-950-0-0.png?k=bc62c7df04"></img> </a>
+        <a  href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img className="banner-ad" src="https://a.ltrbxd.com/resized/sm/upload/j8/gt/r8/ss/pro-950-0-950-0-0.png?k=bc62c7df04"></img> </a>
         <div>
           <span>POPULAR PEOPLE</span>
           <div id="featured-people">
